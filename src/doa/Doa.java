d9e72d6d9c3d4bad37fd6ae0ec87a66ce06940bc
Gabriel Navarro dos Santos <gabrielnavarrodoss@gmail.com>
2025-10-12 19:03:14 -0300

    Criação da Pasta entidades, e das classes Produtos Pedidos, Clientes, Funcionarios

--- src/doa/Doa.java
+++ src/doa/Doa.java
@@ -1,5 +1,5 @@
 package doa;
 
-public class Doa {
+public interface  Doa {
 
 }
---
+++ src/entidades/Clientes.java
@@ -0,0 +1,10 @@
+package entidades;
+
+public class Clientes {
+    
+    
+    
+    public Clientes() {
+
+    }
+}
